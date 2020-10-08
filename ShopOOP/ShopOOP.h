80e4673088f20fa612f2b79aa820ef0804acb1db
AnyDodic <korri.13@inbox.ru>
2020-10-08 03:26:28 +0300

    Initial Commit

---
+++ ShopOOP/ShopOOP.h
@@ -0,0 +1,18 @@
+//
+//  ShopOOP.h
+//  ShopOOP
+//
+//  Created by Alexey on 08.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShopOOP.
+FOUNDATION_EXPORT double ShopOOPVersionNumber;
+
+//! Project version string for ShopOOP.
+FOUNDATION_EXPORT const unsigned char ShopOOPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShopOOP/PublicHeader.h>
+
+
